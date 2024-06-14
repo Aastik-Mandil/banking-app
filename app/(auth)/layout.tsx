aa13e56a46692a0aa8dfcfc9623cd793c94feb7c
Aastik Mandil <astik24mandilkota@gmail.com>
2024-06-14 17:12:31 +0530

    create header, sidebar, rightSidebar

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <main className="flex min-h-screen w-full justify-between font-inter">
+      {children}
+    </main>
+  );
+}
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,33 @@
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 import Image from "next/image";
 
 export default function Home() {
+  const loggedIn = { firstName: "Aastik", lastName:"Mandil", email: "test@gmail.com" };
+
   return (
-    <main className="">
-      Bankin App
-    </main>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title={"Welcome"}
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently"
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+
+        {/* <RecentTransactions /> */}
+      </div>
+
+      <RightSidebar user={loggedIn} transactions={[]} banks={[{},{}]} />
+    </section>
   );
 }
